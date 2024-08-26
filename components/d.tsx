816bb641125cee5e75f233561501880f24b67c1f
sumitdoke <96287752+sumitdoke@users.noreply.github.com>
2024-08-26 09:32:46 +0530

    Update d.tsx

--- components/d.tsx
+++ components/d.tsx
@@ -1,17 +1,1 @@
-// import React from 'react';
-// import Toolbar from './Toolbar';
-// import Grid from './Grid';
 
-// const Layout: React.FC = ({ children }) => {
-//   return (
-//     <div>
-//       <Toolbar />
-//       <main>
-//         {children}
-//         <Grid />
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Layout;
