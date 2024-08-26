d574d2e006e82e033a7e14c426dd147f64f0c81b
sumitdoke <96287752+sumitdoke@users.noreply.github.com>
2024-08-26 09:32:14 +0530

    Update s.tsx

--- components/s.tsx
+++ components/s.tsx
@@ -1,12 +1,1 @@
-// import React from 'react';
-// import Layout from '../components/Layout';
 
-// const Page: React.FC = () => {
-//   return (
-//     <Layout>
-//       {/* Other content can be added here if needed */}
-//     </Layout>
-//   );
-// };
-
-// export default Page;
